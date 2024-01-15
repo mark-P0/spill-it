15bba2730082535c1ab28048a3eff688ad653895
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-16 03:12:25 +0800

    Remove logging on migrations

--- api/data/db.ts
+++ api/data/db.ts
@@ -1,11 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 import { env } from "../utils/env";
-import { localizeLogger } from "../utils/logger";
 
-const logger = localizeLogger(import.meta.url);
-
-logger.info("Connecting to database");
 const pool = new Pool({
   connectionString: env.SUPABASE_POSTGRES_URI_POOLING,
 });
