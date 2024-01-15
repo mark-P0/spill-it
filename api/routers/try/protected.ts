60c79e0ce72d5a45d8a0fcb0d6f15abf9232b4e4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-16 05:45:33 +0800

    Use session UUID to protect routes

--- api/routers/try/protected.ts
+++ api/routers/try/protected.ts
@@ -1,8 +1,9 @@
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
+import { isSessionExpired, readSessionFromUUID } from "../../data/sessions";
 import { endpoints, parseHeaderAuthSession } from "../../utils/express";
 import { localizeLogger } from "../../utils/logger";
-import { safe } from "../../utils/try-catch";
+import { safe, safeAsync } from "../../utils/try-catch";
 import { TryRouter } from "../try";
 
 const logger = localizeLogger(import.meta.url);
@@ -16,7 +17,7 @@ TryRouter.get(endpoints.try.unprotected, (req, res, next) => {
   });
 });
 
-TryRouter.get(endpoints.try.protected, (req, res, next) => {
+TryRouter.get(endpoints.try.protected, async (req, res, next) => {
   logger.info("Parsing headers...");
   const parsingHeaderAuth = safe(() => parseHeaderAuthSession(req.headers));
   if (!parsingHeaderAuth.success) {
@@ -27,10 +28,32 @@ TryRouter.get(endpoints.try.protected, (req, res, next) => {
   }
   const headerAuth = parsingHeaderAuth.value;
 
+  logger.info("Fetching session info...");
   const { id } = headerAuth.params;
-  // TODO if expired(id) UNAUTHORIZED
+  const resultSession = await safeAsync(() => readSessionFromUUID(id));
+  if (!resultSession.success) {
+    logger.error(resultSession.error.stack);
+    return res
+      .status(StatusCodes.BAD_GATEWAY)
+      .json({ error: "Read session failed" });
+  }
 
-  res.status(StatusCodes.NOT_IMPLEMENTED).json({
+  const session = resultSession.value;
+  if (session === null) {
+    logger.info("Session does not exist");
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "Session not found" });
+  }
+  if (isSessionExpired(session)) {
+    logger.info("Session is expired");
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ error: "Session expired" });
+  }
+
+  logger.info("Session verified; providing requested resource...");
+  res.json({
     data: {
       resource: "protected",
       access: id,
