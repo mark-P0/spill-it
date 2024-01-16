3aa8fefab146667d5f34477b642f823d64e71a85
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-16 14:48:22 +0800

    Use identity function as endpoint helper

--- api/routers/try.ts
+++ api/routers/try.ts
@@ -1,25 +1,25 @@
 import { Router } from "express";
 import { getAllSamples } from "../data/samples";
-import { endpoints } from "../utils/express";
+import { endpoint } from "../utils/express";
 import { localizeLogger } from "../utils/logger";
 
 const logger = localizeLogger(import.meta.url);
 export const TryRouter = Router();
 
-TryRouter.get(endpoints.try.hello, (req, res) => {
+TryRouter.get(endpoint("/try/hello"), (req, res) => {
   const { who = "world" } = req.query;
 
   res.json({ hello: `${who}!` });
 });
 
-TryRouter.get(endpoints.try.sample, async (req, res) => {
+TryRouter.get(endpoint("/try/sample"), async (req, res) => {
   const data = await getAllSamples();
   logger.debug("Data: " + JSON.stringify(data, undefined, 1));
 
   res.json({ data });
 });
 
-TryRouter.get(endpoints.try["not-found"]); // Should not be handled by anything as it shouldn't exist :)
-TryRouter.get(endpoints.try.error, () => {
+TryRouter.get(endpoint("/try/not-found")); // Should not be handled by anything as it shouldn't exist :)
+TryRouter.get(endpoint("/try/error"), () => {
   throw new Error("Something went horribly wrong");
 });
--- api/routers/try/ui.ts
+++ api/routers/try/ui.ts
@@ -3,18 +3,18 @@
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
 import { buildAuthUrl } from "../../auth/google";
-import { endpoints } from "../../utils/express";
+import { endpoint } from "../../utils/express";
 import { TryRouter } from "../try";
 
 const redirectUri =
-  "https://localhost:3000" + endpoints.try.ui.login.google.redirect;
+  "https://localhost:3000" + endpoint("/try/ui/login/google/redirect");
 
-TryRouter.get(endpoints.try.ui.login.google["/"], async (req, res, next) => {
+TryRouter.get(endpoint("/try/ui/login/google"), async (req, res, next) => {
   const authUrl = await buildAuthUrl(redirectUri);
 
   res.json({ redirect: authUrl });
 });
-TryRouter.get(endpoints.try.ui.login.google.redirect, (req, res, next) => {
+TryRouter.get(endpoint("/try/ui/login/google/redirect"), (req, res, next) => {
   const parsing = z.object({ code: z.string() }).safeParse(req.query);
   if (!parsing.success) {
     res.status(StatusCodes.BAD_REQUEST).json({ error: "Invalid query params" });
