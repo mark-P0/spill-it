4927f7898752a1cc96290903de4e6eebe9e0a47a
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-19 19:43:25 +0800

    Initialize React Router according to tutorial

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./assets/tailwind.css";
 
+/** https://reactrouter.com/en/main/start/tutorial#adding-a-router */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/hello",
+    element: <div>I'm a new route!</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
