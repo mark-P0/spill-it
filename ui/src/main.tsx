0f774b7df8cf15ccdfd939e5b9de47790c16b607
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-19 20:38:22 +0800

    Try React Router loaders

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,19 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {
+  LoaderFunction,
   Route,
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
+  redirect,
 } from "react-router-dom";
 import { App } from "./App.tsx";
 import "./assets/tailwind.css";
+
+async function sleep(seconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
+
+const loadSearchParamsFromUrl: LoaderFunction = async (arg) => {
+  const { params, request } = arg;
+  const query = Object.fromEntries(new URL(request.url).searchParams);
+
+  console.log({ arg, params, request });
+  console.log(query);
+
+  return redirect("/");
+};
+const loadSleep: LoaderFunction = async () => {
+  await sleep(3);
+  return redirect("/");
+};
+const loadError: LoaderFunction = () => {
+  throw new Error("bruh");
+};
 
 /** https://reactrouter.com/en/main/utils/create-routes-from-elements */
 const routes = createRoutesFromElements(
   <>
     <Route path="/" element={<App />} />
-    <Route path="/hello" element={<div>I'm a new route!</div>} />
+    <Route path="/query" element={null} loader={loadSearchParamsFromUrl} />
+    <Route path="/sleep" element={null} loader={loadSleep} />
+    <Route path="/error" element={null} loader={loadError} />
   </>,
 );
 
