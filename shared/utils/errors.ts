a5b9e086bf242237030fc71001e0e356ba7bb56f
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-20 01:32:02 +0800

    Attach causes manually on error formatter

--- shared/utils/errors.ts
+++ shared/utils/errors.ts
@@ -1,11 +1,41 @@
-import util from "node:util";
+function indent(str: string, amount = 2, char = " "): string {
+  const indention = char.repeat(amount);
+  return indention + str.replace(/\n/g, "\n" + indention);
+}
+
+export function ensureError(value: unknown): Error {
+  if (value instanceof Error) return value;
+
+  let valueStr = "[Thrown value cannot be stringified]";
+  try {
+    valueStr = JSON.stringify(valueStr);
+  } catch {}
+
+  // TODO Log warning to use an Error when throwing?
+  return new Error(`A non-Error was thrown, stringified as: ${valueStr}`);
+}
 
 /**
- * Return `error` as a string like how it would be formatted in `console.log(error)`
+ * Convert `error` into string
+ *
+ * Specifically into its stack message, but if it does not exist
+ * (non-standard property), then just convert it directly.
+ *
+ * Also **manually** attaches the stack messages of the errors that caused it, if any.
+ *
+ * ---
+ *
+ * The ideal version of this function is the output of `require('node:util').format(error)`
+ * in Node.js, i.e. how it would appear in `console.log(error)`
  * - https://nodejs.org/api/console.html#consolelogdata-args
  */
 export function formatError(error: Error): string {
-  return util.format(error);
+  const string = error.stack ?? error.toString();
+  if (error.cause === undefined) {
+    return string;
+  }
+  const cause = ensureError(error.cause);
+  return string + "\n" + indent("[cause] " + formatError(cause));
 }
 
 export function raise(message: string, cause?: Error): never {
--- shared/utils/safe.ts
+++ shared/utils/safe.ts
@@ -1,3 +1,5 @@
+import { ensureError } from "./errors";
+
 /**
  * - https://old.reddit.com/r/reactjs/comments/dvpdgc/how_to_avoid_trycatch_statements_nestingchaining/
  * - https://fsharpforfunandprofit.com/posts/recipe-part2/
@@ -6,18 +8,6 @@
  * - https://www.youtube.com/watch?v=J-HWmoTKhC8
  */
 null;
-
-export function ensureError(value: unknown): Error {
-  if (value instanceof Error) return value;
-
-  let valueStr = "[Thrown value cannot be stringified]";
-  try {
-    valueStr = JSON.stringify(valueStr);
-  } catch {}
-
-  // TODO Log warning to use an Error when throwing?
-  return new Error(`A non-Error was thrown, stringified as: ${valueStr}`);
-}
 
 // type Result<T> = [T, null] | [null, Error]; // Monadic-ish?
 type Result<T> = { success: true; value: T } | { success: false; error: Error }; // Same format as Zod...? Or is it from another source?
