9a23ddbd4705bf0a9ea2aeb87604a83ab2a68c7a
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-24 14:03:00 +0800

    Move login check to utils

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,28 +1,12 @@
-import { buildHeaderAuth } from "@spill-it/header-auth";
 import { Route, redirect } from "react-router-dom";
 import { endpoint } from "./utils/endpoints";
-import { fetchAPI } from "./utils/fetch-api";
-
-async function isSessionValid(): Promise<boolean> {
-  const id = localStorage.getItem("SPILLITSESS");
-  if (id === null) return false;
-
-  const res = await fetchAPI("/api/v0/users/me", {
-    headers: {
-      Authorization: buildHeaderAuth("SPILLITSESS", { id }),
-    },
-  });
-  if (!res.success) return false;
-
-  return true;
-}
+import { isLoggedIn } from "./utils/is-logged-in";
 
 export const RootRoute = () => (
   <Route
     path={endpoint("/")}
     loader={async () => {
-      const isValid = await isSessionValid();
-      if (isValid) return redirect(endpoint("/home"));
+      if (await isLoggedIn()) return redirect(endpoint("/home"));
       return redirect(endpoint("/welcome"));
     }}
     element={null}
