8ba13b6d84077a3be50a58a27740d216445455b9
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-24 14:19:41 +0800

    Wrap routes in an error route

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,6 +2,14 @@ import { Route, redirect } from "react-router-dom";
 import { endpoint } from "./utils/endpoints";
 import { isLoggedIn } from "./utils/is-logged-in";
 
+export function ErrorScreen() {
+  return (
+    <main className="h-screen w-screen grid place-items-center bg-stone-700 text-white">
+      Sorry! We spilt too much. Please try again!
+    </main>
+  );
+}
+
 export const RootRoute = () => (
   <Route
     path={endpoint("/")}
