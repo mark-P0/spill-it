869152ad28ffa101619f359a9512a806ce7072a0
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-29 17:28:01 +0800

    Initialize new API fetching utility

---
+++ ui/src/utils/fetch-api2.ts
@@ -0,0 +1,71 @@
+import {
+  Endpoint,
+  EndpointInput,
+  EndpointMethod,
+  EndpointOutput,
+  endpointMap,
+} from "@spill-it/endpoints/index2";
+import { ensureError, raise } from "@spill-it/utils/errors";
+import { Result } from "@spill-it/utils/safe";
+import { env } from "./env";
+
+const hostAPI = env.DEV
+  ? env.VITE_HOST_API_DEV
+  : env.PROD
+    ? env.VITE_HOST_API_PROD
+    : raise("Impossible situation for API host URL");
+
+function buildRequestFromInput<T extends Endpoint>(
+  endpoint: T,
+  method: EndpointMethod<T>,
+  input: Record<string, Record<string, string>>, // Could be `EndpointInput<T, U>` but that goes crazy...
+): Request {
+  const url = new URL(endpoint, hostAPI);
+  const options: RequestInit = {};
+
+  options.method =
+    typeof method === "string"
+      ? method
+      : raise(`Unknown method ${String(method)} against endpoint ${endpoint}`);
+
+  if (input?.query !== undefined) {
+    for (const [key, value] of Object.entries(input.query)) {
+      url.searchParams.set(key, value);
+    }
+  }
+  if (input?.headers !== undefined) {
+    options.headers = input.headers;
+  }
+  if (input?.body !== undefined) {
+    options.body = JSON.stringify(input.body); // TODO Use `superjson`?
+  }
+
+  return new Request(url.href, options);
+}
+
+export async function fetchAPI2<
+  T extends Endpoint,
+  U extends EndpointMethod<T>,
+>(
+  endpoint: T,
+  method: U,
+  rawInput: EndpointInput<T, U>,
+): Promise<Result<EndpointOutput<T, U>>> {
+  try {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any -- Something about TypeScript does not work after 2 indices... */
+    const signature = endpointMap[endpoint][method] as any;
+
+    const input = signature.input.parse(rawInput);
+    const req = buildRequestFromInput(endpoint, method, input);
+
+    const res = await fetch(req);
+    const rawOutput = await res.json();
+    const output = signature.output.parse(rawOutput);
+    return { success: true, value: output };
+  } catch (caughtError) {
+    const error = new Error("Failed fetching from API", {
+      cause: ensureError(caughtError),
+    });
+    return { success: false, error };
+  }
+}
