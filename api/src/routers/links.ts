398cf219c2576f1f887dd2e7a23eb399d61128be
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-29 17:30:01 +0800

    Delete old links route handler

--- api/src/routers/links.ts
+++ api/src/routers/links.ts
@@ -5,41 +5,12 @@ import { Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
 import { z } from "zod";
 import { buildAuthUrl } from "../../auth/google";
-import { endpointHandler } from "../utils/endpoint-handler";
 import { parseInputFromRequest } from "../utils/endpoints";
 import { localizeLogger } from "../utils/logger";
 
 const logger = localizeLogger(__filename);
 export const LinksRouter = Router();
 
-// LinksRouter.get(
-//   ...endpointHandler("/api/v0/links/google", async (req, res, next) => {
-//     const parsingQuery = z
-//       .object({ redirectUri: z.string().url() })
-//       .safeParse(req.query);
-//     if (!parsingQuery.success) {
-//       logger.error(formatError(parsingQuery.error));
-//       return res
-//         .status(StatusCodes.BAD_REQUEST)
-//         .json({ success: false, error: "Invalid query" });
-//     }
-//     const query = parsingQuery.data;
-//
-//     const resultAuthUrl = await safeAsync(() =>
-//       buildAuthUrl(query.redirectUri),
-//     );
-//     if (!resultAuthUrl.success) {
-//       logger.error(formatError(resultAuthUrl.error));
-//       return res
-//         .status(StatusCodes.BAD_GATEWAY)
-//         .json({ success: false, error: "Create URL failed" });
-//     }
-//     const authUrl = resultAuthUrl.value;
-//
-//     logger.info("Responding with authorization URL...");
-//     res.json({ success: true, link: authUrl });
-//   }),
-// );
 {
   const details = endpointDetails("/api/v0/links/google", "GET");
   const [ep, method, signature, methodLower] = details;
