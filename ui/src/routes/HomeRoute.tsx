004505003b5a53bc6625b5355140bd6f37e4782f
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-31 01:52:31 +0800

    Create posts in UI home route

--- ui/src/routes/HomeRoute.tsx
+++ ui/src/routes/HomeRoute.tsx
@@ -1,11 +1,67 @@
+import { safe } from "@spill-it/utils/safe";
+import { FormEvent, useState } from "react";
 import { Route, redirect } from "react-router-dom";
 import { endpoint } from "../utils/endpoints";
-import { isLoggedIn } from "../utils/is-logged-in";
+import { fetchAPI } from "../utils/fetch-api";
+import { buildHeaderAuthFromStorage, isLoggedIn } from "../utils/is-logged-in";
 
-function HomeScreen() {
+function PostForm() {
+  const [content, setContent] = useState("");
+
+  function reset() {
+    setContent("");
+  }
+
+  // TODO Show a popup on error?
+  async function submit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const headerAuthResult = safe(() => buildHeaderAuthFromStorage());
+    if (!headerAuthResult.success) {
+      console.error(headerAuthResult.error);
+      return;
+    }
+    const headerAuth = headerAuthResult.value;
+
+    const fetchResult = await fetchAPI("/api/v0/posts", "POST", {
+      headers: { Authorization: headerAuth },
+      body: { content },
+    });
+    if (!fetchResult.success) {
+      console.error(fetchResult.error);
+      return;
+    }
+
+    reset();
+  }
+
   return (
-    <main className="h-screen w-screen grid place-items-center bg-stone-700 text-white">
-      Home page
+    <form onSubmit={submit} className="grid gap-3">
+      <label>
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.currentTarget.value)}
+          placeholder="What's the tea sis?!"
+          className="w-full bg-black/20 resize-none p-3"
+        ></textarea>
+      </label>
+
+      <button
+        disabled={content === ""}
+        className="ml-auto bg-rose-500 disabled:opacity-50 rounded-full px-6 py-3 font-bold tracking-wide"
+      >
+        Spill! 🍵
+      </button>
+    </form>
+  );
+}
+
+export function HomeScreen() {
+  return (
+    <main className="min-h-screen bg-fuchsia-950 text-white grid auto-rows-min gap-6 p-6">
+      <h1 className="text-3xl">Home</h1>
+
+      <PostForm />
     </main>
   );
 }
