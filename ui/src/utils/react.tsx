3446ab8f4d480f78d6da186dedd248108da56aac
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-31 21:06:18 +0800

    Define custom context creator

---
+++ ui/src/utils/react.tsx
@@ -0,0 +1,20 @@
+import { raise } from "@spill-it/utils/errors";
+import { PropsWithChildren, createContext, useContext } from "react";
+
+export function createNewContext<T>(useContextValue: () => T) {
+  const NewContext = createContext<T | null>(null);
+
+  function useNewContext(): T {
+    const values =
+      useContext(NewContext) ?? raise("Context possibly not provided");
+    return values;
+  }
+
+  function NewContextProvider(props: PropsWithChildren) {
+    const { children } = props;
+    const value = useContextValue();
+    return <NewContext.Provider value={value}>{children}</NewContext.Provider>;
+  }
+
+  return [useNewContext, NewContextProvider] as const;
+}
