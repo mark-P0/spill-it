9afbc1fd3dbe286a937f81febc082c70bd17168f
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-01-31 20:12:01 +0800

    Use `clsx` to wrap long classes

--- ui/src/routes/HomeRoute.tsx
+++ ui/src/routes/HomeRoute.tsx
@@ -1,4 +1,5 @@
 import { safe } from "@spill-it/utils/safe";
+import clsx from "clsx";
 import { FormEvent, useState } from "react";
 import { Route, redirect } from "react-router-dom";
 import { endpoint } from "../utils/endpoints";
@@ -42,13 +43,18 @@ function PostForm() {
           value={content}
           onChange={(event) => setContent(event.currentTarget.value)}
           placeholder="What's the tea sis?!"
-          className="w-full bg-black/20 resize-none p-3"
+          className={clsx("resize-none", "w-full p-3", "bg-black/20")}
         ></textarea>
       </label>
 
       <button
         disabled={content === ""}
-        className="ml-auto bg-rose-500 disabled:opacity-50 rounded-full px-6 py-3 font-bold tracking-wide"
+        className={clsx(
+          "ml-auto",
+          "rounded-full px-6 py-3",
+          "bg-rose-500 disabled:opacity-50",
+          "font-bold tracking-wide",
+        )}
       >
         Spill! 🍵
       </button>
@@ -58,7 +64,13 @@ function PostForm() {
 
 export function HomeScreen() {
   return (
-    <main className="min-h-screen bg-fuchsia-950 text-white grid auto-rows-min gap-6 p-6">
+    <main
+      className={clsx(
+        "min-h-screen",
+        "grid auto-rows-min gap-6 p-6",
+        "bg-fuchsia-950 text-white",
+      )}
+    >
       <h1 className="text-3xl">Home</h1>
 
       <PostForm />
