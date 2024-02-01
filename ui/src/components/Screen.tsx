bd36895e8b64d1796d916e69a2f769c4e686f362
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-02 07:01:17 +0800

    Transition screens in

--- ui/src/components/Screen.tsx
+++ ui/src/components/Screen.tsx
@@ -1,11 +1,14 @@
 import clsx from "clsx";
-import { ComponentProps, useEffect } from "react";
+import { ComponentProps, useEffect, useState } from "react";
 import { ToastProvider } from "../contexts/toast";
 import { removeBodyClasses, restoreBodyClasses } from "../utils/body-classes";
 import { Toast } from "./Toast";
 
 export function Screen(props: ComponentProps<"div">) {
+  const [isRendered, setIsRendered] = useState(false);
   useEffect(() => {
+    setIsRendered(true);
+
     return () => {
       restoreBodyClasses(); // Unmounting the screen means the body will be seen, in which case the animations should be shown
     };
@@ -14,12 +17,22 @@ export function Screen(props: ComponentProps<"div">) {
   const { children, className, ...attributes } = props;
   return (
     <ToastProvider>
-      <div
-        {...attributes}
-        className={clsx("min-h-screen", "bg-fuchsia-950 text-white", className)}
-        onTransitionEnd={removeBodyClasses} // Animations still "computed" even if screen is in front
-      >
-        {children}
+      <div className="overflow-clip">
+        <div
+          {...attributes}
+          className={clsx(
+            "min-h-screen",
+            "bg-fuchsia-950 text-white",
+            ...[
+              "transition duration-700",
+              !isRendered && "opacity-0 scale-105",
+            ],
+            className,
+          )}
+          onTransitionEnd={removeBodyClasses} // Animations still "computed" even if screen is in front
+        >
+          {children}
+        </div>
       </div>
 
       <Toast />
