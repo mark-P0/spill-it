fd5cc340d45ec87e5f59c1aa652018012aaceae0
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-02 00:07:32 +0800

    Redirect root accesses to home route directly

--- ui/src/routes/_root.tsx
+++ ui/src/routes/_root.tsx
@@ -1,16 +1,10 @@
 import { RouteObject, redirect } from "react-router-dom";
 import { endpoint } from "../utils/endpoints";
-import { isLoggedIn } from "../utils/is-logged-in";
 
 export const RootRoute: RouteObject = {
   path: endpoint("/"),
-  async loader() {
-    const canShowHome = await isLoggedIn();
-    if (canShowHome) {
-      return redirect(endpoint("/home"));
-    }
-
-    return redirect(endpoint("/welcome"));
+  loader() {
+    return redirect(endpoint("/home"));
   },
   element: null,
 };
