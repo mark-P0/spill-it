008e8878bd6706f8032437cf76f8d5475d53a09b
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-02 10:59:12 +0800

    Ensure body animations are removed on screen rerenders

--- ui/src/components/Screen.tsx
+++ ui/src/components/Screen.tsx
@@ -8,11 +8,15 @@ export function Screen(props: ComponentProps<"div">) {
   const [isRendered, setIsRendered] = useState(false);
   useEffect(() => {
     setIsRendered(true);
+  }, []);
 
+  const [hasTransitioned, setHasTransitioned] = useState(false);
+  useEffect(() => {
+    if (hasTransitioned) removeBodyClasses(); // Animations still "computed" even if screen is in front
     return () => {
       restoreBodyClasses(); // Unmounting the screen means the body will be seen, in which case the animations should be shown
     };
-  }, []);
+  }, [hasTransitioned]);
 
   const { children, className, ...attributes } = props;
   return (
@@ -29,7 +33,7 @@ export function Screen(props: ComponentProps<"div">) {
             ],
             className,
           )}
-          onTransitionEnd={removeBodyClasses} // Animations still "computed" even if screen is in front
+          onTransitionEnd={() => setHasTransitioned(true)}
         >
           {children}
         </div>
