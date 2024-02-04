b8ecc0edc24c6c79e466f481215ecb405f656d86
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-04 09:06:13 +0800

    Move home route context to own file for better HMR

---
+++ ui/src/routes/home/HomeContext.ts
@@ -0,0 +1,41 @@
+import { PostWithAuthor } from "@spill-it/db/tables/posts";
+import { safe } from "@spill-it/utils/safe";
+import { useEffect, useState } from "react";
+import { fetchAPI } from "../../utils/fetch-api";
+import { buildHeaderAuthFromStorage } from "../../utils/is-logged-in";
+import { createNewContext } from "../../utils/react";
+
+export const [useHomeContext, HomeProvider] = createNewContext(() => {
+  const [posts, setPosts] = useState<PostWithAuthor[] | "fetching" | "error">(
+    "fetching",
+  );
+
+  async function refreshPosts() {
+    const headerAuthResult = safe(() => buildHeaderAuthFromStorage());
+    if (!headerAuthResult.success) {
+      console.error(headerAuthResult.error);
+      setPosts("error");
+      return;
+    }
+    const headerAuth = headerAuthResult.value;
+
+    const fetchResult = await fetchAPI("/api/v0/posts", "GET", {
+      headers: { Authorization: headerAuth },
+    });
+    if (!fetchResult.success) {
+      console.error(fetchResult.error);
+      setPosts("error");
+      return;
+    }
+    const { data } = fetchResult.value;
+
+    setPosts(data);
+  }
+
+  useEffect(() => {
+    setPosts("fetching");
+    refreshPosts();
+  }, []);
+
+  return { posts, refreshPosts };
+});
