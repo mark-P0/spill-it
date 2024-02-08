9bffae54389735ef496e4523bad47afee312576c
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-09 04:32:34 +0800

    AMENDME

--- ui/src/routes/try.tsx
+++ ui/src/routes/try.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+import { useEffect, useRef } from "react";
 import { RouteObject, redirect } from "react-router-dom";
 import { Screen } from "./_app/Screen";
 import { ModalContent } from "./_app/modal/Modal";
@@ -88,10 +90,134 @@ const modalRoute: RouteObject = (() => {
   };
 })();
 
+type Tag = keyof HTMLElementTagNameMap; // From signature of `document.createElement()`
+type AttributeValue = string | boolean;
+type Child = string | Node; // From signature of `Element.replaceChildren()`
+
+function E<T extends Tag>(
+  tag: T,
+  attributes: Record<string, AttributeValue>,
+  children: Child[],
+): HTMLElementTagNameMap[T] {
+  const element = document.createElement(tag);
+  return EE(element, attributes, children);
+}
+function EE<T extends HTMLElement>(
+  element: T,
+  attributes: Record<string, AttributeValue>,
+  children: Child[],
+): T {
+  for (const [name, givenValue] of Object.entries(attributes)) {
+    let value = givenValue;
+
+    /**
+     * https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
+     * - Boolean attributes set to `true` should be set to an empty string
+     * - Boolean attributes set to `false` should NOT be present in the element at all
+     */
+    if (typeof value === "boolean") {
+      if (!value) continue;
+      value = "";
+    }
+
+    element.setAttribute(name, value);
+  }
+
+  element.replaceChildren(...children);
+
+  return element;
+}
+
+const googleLoginButtonRoute: RouteObject = (() => {
+  function Element() {
+    const divRef = useRef<HTMLDivElement | null>(null);
+    useEffect(() => {
+      const div = divRef.current;
+      if (div == null) return;
+
+      EE(div, {}, [
+        E(
+          "script",
+          { src: "https://accounts.google.com/gsi/client", async: true },
+          [],
+        ),
+      ]);
+    }, []);
+
+    return (
+      <Screen>
+        <div
+          className="flex" // Used to contain the sign-in button as it seems to resize on its own...
+        >
+          <div className="relative overflow-clip w-min h-min">
+            {
+              /**
+               * Ideally this script element from Google should be used.
+               * However React does not seem to "render" script elements,
+               * so it is added via regular DOM manipulation into the `<div>` container that follows.
+               *
+               * The location of the script element does not seem to matter,
+               * however it seems to work only if the elements are actually
+               * already in the DOM. This can be tricky because of e.g. React rerenders.
+               */
+              // <script src="https://accounts.google.com/gsi/client" async></script>
+
+              <div ref={divRef}></div>
+            }
+
+            {
+              /**
+               * Can configure the button visually using Google's tool
+               *
+               * https://developers.google.com/identity/gsi/web/tools/configurator
+               */
+              <>
+                <div
+                  id="g_id_onload"
+                  data-client_id="1087789624520-nkoblr8n59th42s76gqrok4useaq2ojj.apps.googleusercontent.com"
+                  // data-login_uri="https://your.domain/your_login_endpoint"
+                  data-auto_prompt="false"
+                ></div>
+                <div
+                  className="g_id_signin"
+                  data-type="standard"
+                  data-size="large"
+                  data-theme="outline"
+                  data-text="continue_with"
+                  data-shape="rectangular"
+                  data-logo_alignment="left"
+                ></div>
+              </>
+            }
+
+            {/** The following is laid on top of the Google sign-in button to "override" its behavior. */}
+            <button
+              onClick={() => console.warn("Actually login!")}
+              className={clsx(
+                "absolute top-0 left-0 w-full h-full",
+                // "bg-blue-500/50",
+                "bg-transparent",
+              )}
+            >
+              {/* I should be invisible */}
+            </button>
+          </div>
+        </div>
+      </Screen>
+    );
+  }
+
+  return {
+    path: "/google",
+    element: <Element />,
+  };
+})();
+
 export const tryRoutes: RouteObject[] = [
   queryRoute,
   sleepRoute,
   loadRoute,
   toastRoute,
   modalRoute,
+  googleLoginButtonRoute,
 ];
