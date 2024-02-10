0e741fed5f6a848d0bfde084124d034e3d86424a
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-10 17:58:57 +0800

    AMENDME

--- shared/db/schema.ts
+++ shared/db/schema.ts
@@ -15,14 +15,14 @@
  *
  * https://stackoverflow.com/questions/33274291/uuid-or-sequence-for-primary-key
  * - It is fine to use UUIDs as Primary Key
- * - UUIDs could be inefficient in terms of computation (random generation) and storage (needs more bytes)
+ * - UUIDs could be inefficient in terms of computation (random Inferrederation) and storage (needs more bytes)
  * - Correctly implemented UUIDs should be secure and random enough
- * - Postgres UUID generation might not be secure and random enough
+ * - Postgres UUID Inferrederation might not be secure and random enough
  *
  * https://cheatsheetseries.owasp.org/cheatsheets/Session_Management_Cheat_Sheet.html#session-id-entropy
  * - IDs (e.g. session IDs) should be
  *   - Random
- *     - UUIDv4 should be generated with a CSPRNG
+ *     - UUIDv4 should be Inferrederated with a CSPRNG
  *   - Unique
  *     - Primary Key constraint
  */
@@ -45,7 +45,12 @@ export const SamplesTable = pgTable("samples", {
   fullName: text("fullName"),
   phone: varchar("phone", { length: 256 }),
 });
-export const zodSample = createSelectSchema(SamplesTable);
+const zodSampleInferred = createSelectSchema(SamplesTable);
+export const zodSample: typeof zodSampleInferred = z.object({
+  id: z.string().uuid(),
+  fullName: z.string().nullable(),
+  phone: z.string().nullable(),
+});
 export type Sample = typeof SamplesTable.$inferSelect;
 export type SampleDetails = typeof SamplesTable.$inferInsert;
 
@@ -57,7 +62,15 @@ export const UsersTable = pgTable("users", {
   googleId: text("googleId"),
   loginCt: integer("loginCt").notNull(),
 });
-export const zodUser = createSelectSchema(UsersTable);
+const zodUserInferred = createSelectSchema(UsersTable);
+export const zodUser: typeof zodUserInferred = z.object({
+  id: z.string().uuid(),
+  username: z.string(),
+  handleName: z.string(),
+  portraitUrl: z.string(),
+  googleId: z.string().nullable(),
+  loginCt: z.number(),
+});
 export type User = typeof UsersTable.$inferSelect;
 export type UserDetails = typeof UsersTable.$inferInsert;
 
@@ -66,7 +79,12 @@ export const SessionsTable = pgTable("sessions", {
   userId: uuid("userId").notNull(),
   expiry: timestamp("expiry").notNull(),
 });
-export const zodSession = createSelectSchema(SessionsTable);
+const zodSessionInferred = createSelectSchema(SessionsTable);
+export const zodSession: typeof zodSessionInferred = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  expiry: z.date(),
+});
 export type Session = typeof SessionsTable.$inferSelect;
 export type SessionDetails = typeof SessionsTable.$inferInsert;
 {
@@ -93,7 +111,13 @@ export const PostsTable = pgTable("posts", {
   timestamp: timestamp("timestamp").notNull().defaultNow(),
   content: text("content").notNull(),
 });
-export const zodPost = createSelectSchema(PostsTable);
+const zodPostInferred = createSelectSchema(PostsTable);
+export const zodPost: typeof zodPostInferred = z.object({
+  id: z.string().uuid(),
+  userId: z.string().uuid(),
+  timestamp: z.date(),
+  content: z.string(),
+});
 export type Post = typeof PostsTable.$inferSelect;
 export type PostDetails = typeof PostsTable.$inferInsert;
 {
