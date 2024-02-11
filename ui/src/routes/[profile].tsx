0716bac26478220e3efc9111368500a2af8650ed
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-12 05:25:36 +0800

    Consolidate route definitions to a single file

--- ui/src/routes/[profile].tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import { ProfileScreen } from "./[profile]/ProfileScreen";
-import { loadProfile, profilePath } from "./[profile]/load-profile";
-
-export const ProfileRoute: RouteObject = {
-  path: profilePath,
-  loader: loadProfile,
-  element: <ProfileScreen />,
-};
--- ui/src/routes/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import { ErrorScreen } from "./_app/ErrorScreen";
-
-export const AppRoute = ({ children }: RouteObject): RouteObject => ({
-  errorElement: <ErrorScreen />,
-  children,
-});
