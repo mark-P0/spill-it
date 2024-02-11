1c623dec6dcafcc04938304ede9f34282604c397
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-12 02:49:02 +0800

    Display user info in profile screen

--- ui/src/routes/[profile]/ProfileScreen.tsx
+++ ui/src/routes/[profile]/ProfileScreen.tsx
@@ -3,11 +3,36 @@ import { useProfileLoader } from "./load-profile";
 
 export function ProfileScreen() {
   const profile = useProfileLoader();
-  console.warn({ profile });
+  const { handleName, username, portraitUrl } = profile;
+
+  document.title = `${handleName} (${username}) 👀 Spill.it!`;
 
   return (
-    <Screen>
-      <pre>{JSON.stringify(profile, undefined, 2)}</pre>
+    <Screen className="grid auto-rows-min gap-6 p-6">
+      <header className="grid grid-rows-subgrid row-span-2">
+        <nav className="flex justify-between">
+          <pre>logout</pre>
+          <pre>home</pre>
+        </nav>
+
+        <section className="flex justify-between">
+          <div>
+            <h1 className="text-3xl font-bold">{handleName}</h1>
+            <p className="text-lg text-white/50">{username}</p>
+          </div>
+          <div>
+            <img
+              src={portraitUrl}
+              alt={`Portrait of "${handleName}"`}
+              className="w-20 aspect-square rounded-full"
+            />
+          </div>
+        </section>
+      </header>
+
+      <main>
+        <pre>posts</pre>
+      </main>
     </Screen>
   );
 }
