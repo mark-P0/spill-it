f7d317cf151f889a1099de8a5bec523fbdf88452
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-12 02:49:48 +0800

    Add button link to home page from profiles

--- ui/src/routes/[profile]/ProfileScreen.tsx
+++ ui/src/routes/[profile]/ProfileScreen.tsx
@@ -1,5 +1,23 @@
+import clsx from "clsx";
+import { BsHouseFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import { endpoint } from "../../utils/endpoints";
 import { Screen } from "../_app/Screen";
 import { useProfileLoader } from "./load-profile";
+
+function HomeButtonLink() {
+  return (
+    <Link
+      to={endpoint("/home")}
+      className={clsx(
+        "w-9 aspect-square rounded-full p-2",
+        ...["transition", "hover:bg-white/25 active:scale-90"],
+      )}
+    >
+      <BsHouseFill className="w-full h-full" />
+    </Link>
+  );
+}
 
 export function ProfileScreen() {
   const profile = useProfileLoader();
@@ -10,9 +28,10 @@ export function ProfileScreen() {
   return (
     <Screen className="grid auto-rows-min gap-6 p-6">
       <header className="grid grid-rows-subgrid row-span-2">
-        <nav className="flex justify-between">
+        <nav className="flex justify-between items-center">
           <pre>logout</pre>
-          <pre>home</pre>
+
+          <HomeButtonLink />
         </nav>
 
         <section className="flex justify-between">
