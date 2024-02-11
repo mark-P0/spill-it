5aa4f5d11b584a61c9823a3a5e1f1ee8b55a8db7
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-11 21:30:36 +0800

    Wrap screens with user context

--- ui/src/routes/_app/Screen.tsx
+++ ui/src/routes/_app/Screen.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { ComponentProps, useEffect, useState } from "react";
 import { removeBodyClasses, restoreBodyClasses } from "../../utils/dom";
+import { UserProvider } from "./UserContext";
 import { Modal } from "./modal/Modal";
 import { ModalProvider } from "./modal/ModalContext";
 import { Toast } from "./toast/Toast";
@@ -22,32 +23,34 @@ export function Screen(props: ComponentProps<"div">) {
 
   const { children, className, ...attributes } = props;
   return (
-    <ToastProvider>
-      <ModalProvider>
-        {/** "Container" for hiding overflow of actual screen  */}
-        <div className="overflow-clip">
-          {/** Actual screen */}
-          <div
-            {...attributes}
-            onTransitionEnd={() => setHasTransitioned(true)}
-            className={clsx(
-              "min-h-screen",
-              "bg-fuchsia-950 text-white",
-              ...[
-                "transition duration-700",
-                !isRendered && "opacity-0 scale-105",
-              ],
-              className,
-            )}
-          >
-            {children}
+    <UserProvider>
+      <ToastProvider>
+        <ModalProvider>
+          {/** "Container" for hiding overflow of actual screen  */}
+          <div className="overflow-clip">
+            {/** Actual screen */}
+            <div
+              {...attributes}
+              onTransitionEnd={() => setHasTransitioned(true)}
+              className={clsx(
+                "min-h-screen",
+                "bg-fuchsia-950 text-white",
+                ...[
+                  "transition duration-700",
+                  !isRendered && "opacity-0 scale-105",
+                ],
+                className,
+              )}
+            >
+              {children}
+            </div>
           </div>
-        </div>
 
-        <Modal />
-      </ModalProvider>
+          <Modal />
+        </ModalProvider>
 
-      <Toast />
-    </ToastProvider>
+        <Toast />
+      </ToastProvider>
+    </UserProvider>
   );
 }
