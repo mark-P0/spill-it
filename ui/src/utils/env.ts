05ee046d667ad7af76e9b5d8d7e7c5278b25d1c7
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-13 06:04:01 +0800

    Create Pino logger with level from env

--- ui/src/utils/env.ts
+++ ui/src/utils/env.ts
@@ -1,6 +1,11 @@
 import { raise } from "@spill-it/utils/errors";
+import { zodOfType } from "@spill-it/utils/zod";
+import { LevelWithSilent } from "pino";
 import { z } from "zod";
 
+const zodPinoLevel = zodOfType<LevelWithSilent>()(
+  z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
+);
 const parsing = z
   .object({
     /** Guaranteed from Vite (https://vitejs.dev/guide/env-and-mode) */
@@ -12,18 +17,19 @@ const parsing = z
       SSR: z.boolean(),
     },
 
-    // TODO Hide dev hosts? e.g. by removing prefixes?
-    /**
-     * Also in `@spill-it/api`
-     *
-     * Prefixed with `VITE` to be included in the build
-     */
+    /** Prefixed with `VITE` to be included in the build */
     ...{
-      VITE_AUTH_GOOGLE_CLIENT_ID: z.string(),
-      VITE_HOST_API_DEV: z.string().url(),
-      VITE_HOST_API_PROD: z.string().url(),
-      VITE_HOST_UI_DEV: z.string().url(),
-      VITE_HOST_UI_PROD: z.string().url(),
+      VITE_PINO_LEVEL: zodPinoLevel,
+
+      // TODO Hide dev hosts? e.g. by removing prefixes?
+      /** Also in `@spill-it/api` */
+      ...{
+        VITE_AUTH_GOOGLE_CLIENT_ID: z.string(),
+        VITE_HOST_API_DEV: z.string().url(),
+        VITE_HOST_API_PROD: z.string().url(),
+        VITE_HOST_UI_DEV: z.string().url(),
+        VITE_HOST_UI_PROD: z.string().url(),
+      },
     },
   })
   .safeParse(import.meta.env);
