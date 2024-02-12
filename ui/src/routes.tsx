08aff185feb6ad8f405684ab0247c939b783fff4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-13 06:05:48 +0800

    Use Pino logging in route loaders

--- ui/src/routes.tsx
+++ ui/src/routes.tsx
@@ -11,6 +11,7 @@ import { redirectUri } from "./routes/welcome/redirect-uri";
 import { endpoint } from "./utils/endpoints";
 import { fetchAPI } from "./utils/fetch-api";
 import { isLoggedIn } from "./utils/is-logged-in";
+import { logger } from "./utils/logger";
 import { deleteFromStorage, setOnStorage } from "./utils/storage";
 
 export const ProfileRoute: RouteObject = {
@@ -22,11 +23,14 @@ export const ProfileRoute: RouteObject = {
 export const HomeRoute: RouteObject = {
   path: endpoint("/home"),
   async loader() {
+    logger.debug("Checking if logged in...");
     const canShowHome = await isLoggedIn();
     if (!canShowHome) {
+      logger.info("Is not logged in; redirecting to welcome page...");
       return redirect(endpoint("/welcome"));
     }
 
+    logger.info("Showing home page...");
     return null;
   },
   element: <HomeScreen />,
@@ -35,7 +39,10 @@ export const HomeRoute: RouteObject = {
 export const LogoutRoute: RouteObject = {
   path: endpoint("/logout"),
   loader() {
+    logger.debug("Deleting session info...");
     deleteFromStorage("SESS");
+
+    logger.info("Redirecting to site root...");
     return redirect(endpoint("/"));
   },
   element: null,
@@ -44,6 +51,7 @@ export const LogoutRoute: RouteObject = {
 export const LoginGoogleRedirectRoute: RouteObject = {
   path: endpoint("/login/google/redirect"),
   async loader({ request }) {
+    logger.debug("Parsing query string...");
     /** https://github.com/remix-run/react-router/issues/9171#issuecomment-1220717197 */
     const query = Object.fromEntries(new URL(request.url).searchParams);
     const parsing = z.object({ code: z.string() }).safeParse(query);
@@ -54,23 +62,23 @@ export const LoginGoogleRedirectRoute: RouteObject = {
           parsing.error,
         );
 
-    /** Convert authorization code to session ID */
-    {
-      const result = await fetchAPI("/api/v0/sessions", "GET", {
-        headers: {
-          Authorization: buildHeaderAuth("SPILLITGOOGLE", {
-            code,
-            redirectUri,
-          }),
-        },
-      });
-      const { Authorization } = result.success
-        ? result.value
-        : raise("Failed retrieving session authorization", result.error);
+    logger.debug("Submitting authorization code for session info...");
+    const result = await fetchAPI("/api/v0/sessions", "GET", {
+      headers: {
+        Authorization: buildHeaderAuth("SPILLITGOOGLE", {
+          code,
+          redirectUri,
+        }),
+      },
+    });
+    const { Authorization } = result.success
+      ? result.value
+      : raise("Failed retrieving session authorization", result.error);
 
-      setOnStorage("SESS", Authorization);
-    }
+    logger.debug("Storing session info...");
+    setOnStorage("SESS", Authorization);
 
+    logger.info("Redirecting to site root...");
     return redirect(endpoint("/"));
   },
   element: null,
@@ -78,14 +86,14 @@ export const LoginGoogleRedirectRoute: RouteObject = {
 export const WelcomeRoute: RouteObject = {
   path: endpoint("/welcome"),
   async loader() {
-    /** Redirect if already logged in */
-    {
-      const canShowHome = await isLoggedIn();
-      if (canShowHome) {
-        return redirect(endpoint("/home"));
-      }
+    logger.debug("Checking if logged in...");
+    const canShowHome = await isLoggedIn();
+    if (canShowHome) {
+      logger.info("Is logged in; redirecting to home page...");
+      return redirect(endpoint("/home"));
     }
 
+    logger.info("Showing welcome page...");
     return null;
   },
   element: <WelcomeScreen />,
@@ -94,6 +102,7 @@ export const WelcomeRoute: RouteObject = {
 export const RootRoute: RouteObject = {
   path: endpoint("/"),
   loader() {
+    logger.info("Redirecting to home page...");
     return redirect(endpoint("/home"));
   },
   element: null,
