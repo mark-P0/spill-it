f8c91f3df15f7391653c59ba94a0a3c912b65f53
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-16 05:52:40 +0800

    Initialize followers list

--- ui/src/routes/[profile]/FollowsModal.tsx
+++ ui/src/routes/[profile]/FollowsModal.tsx
@@ -1,10 +1,55 @@
+import { UserPublic } from "@spill-it/db/schema/drizzle";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Modal, ModalContent } from "../_app/modal/Modal";
 import { ModalProvider, useModalContext } from "../_app/modal/ModalContext";
+import { useProfileContext } from "./ProfileContext";
+
+function UserCard(props: { user: UserPublic }) {
+  const { user } = props;
+
+  const { handleName, username, portraitUrl } = user;
+  return (
+    <article className="grid grid-cols-[auto_1fr_auto] gap-3 bg-white/10 p-3">
+      <div>
+        <img
+          src={portraitUrl}
+          alt={`Portrait of "${handleName}"`}
+          className="w-9 aspect-square rounded-full"
+        />
+      </div>
+      <header>
+        <h3 className="font-bold">{handleName}</h3>
+        <p className="text-white/50 text-sm">{username}</p>
+      </header>
+      <div></div>
+    </article>
+  );
+}
 
 function FollowersModalContent() {
-  return <ModalContent>followers list</ModalContent>;
+  const { profile } = useProfileContext();
+
+  if (profile === null) return null;
+  const { handleName, followers } = profile;
+
+  return (
+    <ModalContent>
+      <h2 className="text-xl">
+        Follows <span className="font-bold">{handleName}</span>
+      </h2>
+
+      <ol className="mt-3">
+        {followers.map(({ follower }) => {
+          return (
+            <li key={follower.id}>
+              <UserCard user={follower} />
+            </li>
+          );
+        })}
+      </ol>
+    </ModalContent>
+  );
 }
 function _FollowersModal() {
   const navigate = useNavigate();
