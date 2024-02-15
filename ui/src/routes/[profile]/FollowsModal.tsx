4c89460c83280445095c22113053601e0c6ca186
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-16 04:55:58 +0800

    Render separate modal on profile followers route

--- ui/src/routes/[profile]/FollowsModal.tsx
+++ ui/src/routes/[profile]/FollowsModal.tsx
@@ -1,5 +1,24 @@
-export function FollowersModal() {
-  console.warn("show popup");
+import { useEffect } from "react";
+import { Modal, ModalContent } from "../_app/modal/Modal";
+import { ModalProvider, useModalContext } from "../_app/modal/ModalContext";
+
+function FollowersModalContent() {
+  return <ModalContent>followers list</ModalContent>;
+}
+function _FollowersModal() {
+  const { showOnModal } = useModalContext();
+  useEffect(() => {
+    showOnModal(<FollowersModalContent />);
+  }, [showOnModal]);
 
   return null;
+}
+export function FollowersModal() {
+  return (
+    <ModalProvider>
+      <_FollowersModal />
+
+      <Modal />
+    </ModalProvider>
+  );
 }
