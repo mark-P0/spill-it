d0dd260a24cb43cc1bd279effc12df32122b4b81
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-17 08:01:37 +0800

    Travel back up route on modal dismiss

--- ui/src/routes/_app/modal/ModalContext.ts
+++ ui/src/routes/_app/modal/ModalContext.ts
@@ -18,9 +18,31 @@ export const [useModalContext, ModalProvider] = createNewContext(() => {
     [],
   );
 
+  /**
+   * Must pass function state in another function, otherwise it will be considered as an "updater function"
+   *
+   * e.g.
+   * ```ts
+   * setOnDismiss(() => () => {
+   *   // Callback logic here
+   * })
+   *
+   * setonDismiss(function updater() {
+   *   return function callback() {
+   *     // Callback logic here
+   *   }
+   * })
+   * ```
+   *
+   * - https://medium.com/swlh/how-to-store-a-function-with-the-usestate-hook-in-react-8a88dd4eede1
+   * - https://react.dev/reference/react/useState#setstate-parameters
+   */
+  const [onDismiss, setOnDismiss] = useState<(() => void) | null>(null);
+
   return {
     ...{ content, showOnModal },
     ...{ isOpen, openModal, closeModal },
     ...{ isCancellable, makeModalCancellable },
+    ...{ onDismiss, setOnDismiss },
   };
 });
