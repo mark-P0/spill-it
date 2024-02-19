1489f819368adf3cb47eb2e06f59d33ae9786feb
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-20 01:49:00 +0800

    AMENDME wrap form contents in fieldset | initialize form submit callback | associate profile form fields with state

--- ui/src/routes/[profile]/ProfileModals.tsx
+++ ui/src/routes/[profile]/ProfileModals.tsx
@@ -1,9 +1,11 @@
 import { UserPublic } from "@spill-it/db/schema/drizzle";
+import { ensureError, raise } from "@spill-it/utils/errors";
 import clsx from "clsx";
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { BsXLg } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { endpointWithParam } from "../../utils/endpoints";
+import { logger } from "../../utils/logger";
 import { Modal, ModalContent } from "../_app/modal/Modal";
 import { ModalProvider, useModalContext } from "../_app/modal/ModalContext";
 import { useProfileContext } from "./ProfileContext";
@@ -154,72 +156,123 @@ export function FollowingModal() {
   );
 }
 
+async function sleep(seconds: number) {
+  return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+}
 function EditProfileModalContent() {
   const { profile } = useProfileContext();
-  const { closeModal } = useModalContext();
+  const { closeModal, makeModalCancellable } = useModalContext();
+
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    if (profile === null) return;
+    setUsername(profile.username);
+  }, [profile]);
+
+  const [handleName, setHandleName] = useState("");
+  useEffect(() => {
+    if (profile === null) return;
+    setHandleName(profile.handleName);
+  }, [profile]);
 
   if (profile === null) return null;
 
+  async function save(event: FormEvent) {
+    event.preventDefault();
+
+    let isSuccess = true;
+    setIsProcessing(true);
+    makeModalCancellable(false);
+    try {
+      await sleep(1);
+      raise("TODO");
+    } catch (caughtError) {
+      isSuccess = false;
+      logger.error(ensureError(caughtError));
+    }
+    setIsProcessing(false);
+    makeModalCancellable(true);
+    if (!isSuccess) return;
+
+    closeModal();
+  }
+
+  const isFormUnedited =
+    username === profile.username && handleName === profile.handleName;
+
   return (
     <ModalContent>
-      <form className="grid gap-6 group">
-        <header className="flex gap-6">
-          <h2 className="text-3xl font-bold">Edit Profile</h2>
+      <form onSubmit={save}>
+        <fieldset disabled={isProcessing} className="grid gap-6">
+          <header className="flex gap-6">
+            <h2 className="text-3xl font-bold">Edit Profile</h2>
 
-          <div className="ml-auto">
+            <div className="ml-auto">
+              <button
+                type="button"
+                onClick={closeModal}
+                className={clsx(
+                  "w-9 aspect-square rounded-full p-2",
+                  ...[
+                    "transition",
+                    "disabled:opacity-50 enabled:active:scale-90 enabled:hover:bg-white/25 ",
+                  ],
+                )}
+              >
+                <BsXLg className="w-full h-full" />
+              </button>
+            </div>
+          </header>
+
+          <div className="grid gap-3">
+            <label className="grid gap-1 group/input">
+              <span className="text-xs uppercase tracking-wide transition text-white/50 group-focus-within/input:text-white">
+                Username
+              </span>
+              <input
+                type="text"
+                name="username"
+                value={username}
+                onChange={(event) => setUsername(event.target.value)}
+                className="rounded px-2 py-1 bg-transparent transition border border-white/25 group-focus-within/input:text-white"
+              />
+            </label>
+
+            <label className="grid gap-1 group/input">
+              <span className="text-xs uppercase tracking-wide transition text-white/50 group-focus-within/input:text-white">
+                Handle Name
+              </span>
+              <input
+                type="text"
+                name="handleName"
+                value={handleName}
+                onChange={(event) => setHandleName(event.target.value)}
+                className="rounded px-2 py-1 bg-transparent transition border border-white/25 group-focus-within/input:text-white"
+              />
+            </label>
+          </div>
+
+          <footer className="flex flex-row-reverse gap-3">
             <button
-              type="button"
-              onClick={closeModal}
+              disabled={isFormUnedited}
               className={clsx(
-                "w-9 aspect-square rounded-full p-2",
-                ...["transition", "hover:bg-white/25 active:scale-90"],
+                "select-none",
+                "rounded-full px-6 py-3",
+                "font-bold tracking-wide",
+                ...[
+                  "transition",
+                  "disabled:opacity-50",
+                  "enabled:active:scale-95",
+                  "bg-fuchsia-500 enabled:hover:bg-fuchsia-600",
+                ],
               )}
             >
-              <BsXLg className="w-full h-full" />
+              Save
             </button>
-          </div>
-        </header>
-
-        <fieldset className="grid gap-3">
-          <label className="grid gap-1 group/input">
-            <span className="text-xs uppercase tracking-wide transition text-white/50 group-focus-within/input:text-white">
-              Username
-            </span>
-            <input
-              type="text"
-              className="rounded px-2 py-1 bg-transparent transition border border-white/25 group-focus-within/input:text-white"
-            />
-          </label>
-
-          <label className="grid gap-1 group/input">
-            <span className="text-xs uppercase tracking-wide transition text-white/50 group-focus-within/input:text-white">
-              Handle Name
-            </span>
-            <input
-              type="text"
-              className="rounded px-2 py-1 bg-transparent transition border border-white/25 group-focus-within/input:text-white"
-            />
-          </label>
+          </footer>
         </fieldset>
-
-        <footer className="flex flex-row-reverse gap-3">
-          <button
-            type="button"
-            className={clsx(
-              "select-none",
-              "rounded-full px-6 py-3",
-              "font-bold tracking-wide",
-              ...[
-                "transition",
-                "disabled:opacity-50",
-                "enabled:active:scale-95",
-                "bg-fuchsia-500 enabled:hover:bg-fuchsia-600",
-              ],
-            )}
-          >
-            Save
-          </button>
-        </footer>
       </form>
     </ModalContent>
   );
