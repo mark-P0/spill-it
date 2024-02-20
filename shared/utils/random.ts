3b600b888b3c52f502f52503a200518a252b616f
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-21 06:20:10 +0800

    Node crypto approach for generating random integers

--- shared/utils/random.ts
+++ shared/utils/random.ts
@@ -5,6 +5,13 @@ export function randomFloat(from: number, to: number): number {
   const range = to - from;
   return Math.random() * range + from;
 }
+/**
+ * Crypto-based approach likely better
+ * - https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+ *
+ * Also, Node has a Crypto function specifically for random integers
+ * - https://nodejs.org/api/crypto.html#cryptorandomintmin-max-callback
+ */
 export function randomInteger(from: number, to: number): number {
   return Math.floor(randomFloat(from, to));
 }
