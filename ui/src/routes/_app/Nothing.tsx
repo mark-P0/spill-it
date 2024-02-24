0f827a186b74d506851bcf60e239591286ccfeaf
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-25 04:44:03 +0800

    Move empty placeholder component to own file

---
+++ ui/src/routes/_app/Nothing.tsx
@@ -0,0 +1,15 @@
+/**
+ * Can be used to place an "element" (it is a text node) where desired but not necessarily "visible"
+ *
+ * Alternative entities include:
+ * - `&nbsp;`
+ * - `&#8203;` Zero-width space
+ *
+ * ---
+ *
+ * - https://github.com/typora/typora-issues/issues/4136
+ * - https://levelup.gitconnected.com/the-zero-width-space-77543a28c984
+ */
+export function Nothing() {
+  return <>&zwj;</>;
+}
