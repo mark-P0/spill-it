fae79b1ab586d0c8cbbe9fa7f659b5e15ff66e3e
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-02-24 19:56:12 +0800

    Use loader in profile screen

--- ui/src/routes/[profile]/ProfileScreen.tsx
+++ ui/src/routes/[profile]/ProfileScreen.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { useProfileLoader } from "../[profile]";
 import { Screen } from "../_app/Screen";
 import { NavBar, ProfileCard } from "./ProfileComponents";
-import { ProfileProvider, useProfileContext } from "./ProfileContext";
+import { ProfileProvider } from "./ProfileContext";
 import { PostsProvider } from "./posts/PostsContext";
 import { PostsList } from "./posts/PostsList";
 
 function _ProfileScreen() {
-  const { profile } = useProfileContext();
-  if (profile !== null) {
-    const { handleName, username } = profile;
-    document.title = `${handleName} (${username}) 👀 Spill.it!`;
-  }
+  const { profile } = useProfileLoader();
+
+  const { handleName, username } = profile;
+  document.title = `${handleName} (${username}) 👀 Spill.it!`;
 
   return (
     <Screen className="grid auto-rows-min gap-6 p-6">
