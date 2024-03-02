30e21902bfc9ddf75f05ae190a663753417a5dc3
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-03-02 08:35:51 +0800

    Allow specifying custom validity on textarea abstraction

--- ui/src/routes/_app/Input.tsx
+++ ui/src/routes/_app/Input.tsx
@@ -35,8 +35,22 @@ export function Input(
  *   - Purely visual styles, e.g. background color shouldn't matter, but also shouldn't hurt to reapply
  * - Very similar to the bold-normal font weight transition trick (overlap and opacity)
  */
-export function TextArea(props: ComponentPropsWithoutRef<"textarea">) {
-  const { ...attributes } = props;
+export function TextArea(
+  props: ComponentPropsWithoutRef<"textarea"> & {
+    validity?: string;
+    reportValidity?: boolean;
+  },
+) {
+  const { validity, reportValidity, ...attributes } = props;
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  useEffect(() => {
+    const textarea = textareaRef.current;
+    if (textarea === null) return;
+
+    textarea.setCustomValidity(validity ?? "");
+    if (reportValidity) textarea.reportValidity();
+  }, [validity, reportValidity]);
 
   return (
     <div className="grid *:row-[1] *:col-[1]">
