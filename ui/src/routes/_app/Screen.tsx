7c522992a693decb348ca87c6e59386ee40a16e2
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-03-02 12:44:15 +0800

    Wrap screen base in a fragment

--- ui/src/routes/_app/Screen.tsx
+++ ui/src/routes/_app/Screen.tsx
@@ -18,23 +18,26 @@ export function Screen(props: ComponentPropsWithoutRef<"div">) {
 
   const { children, className, ...attributes } = props;
   return (
-    <div
-      // "Container" for hiding overflow of actual screen
-      className="overflow-clip"
-    >
-      {/** Actual screen */}
-      <div
-        {...attributes}
-        onTransitionEnd={() => setHasTransitioned(true)}
-        className={clsx(
-          "min-h-screen",
-          "bg-fuchsia-950 text-white",
-          ...["transition duration-700", !isRendered && "opacity-0 scale-105"],
-          className,
-        )}
-      >
-        {children}
+    <>
+      {/** "Container" for hiding overflow of actual screen */}
+      <div className="overflow-clip">
+        {/** Actual screen */}
+        <div
+          {...attributes}
+          onTransitionEnd={() => setHasTransitioned(true)}
+          className={clsx(
+            "min-h-screen",
+            "bg-fuchsia-950 text-white",
+            ...[
+              "transition duration-700",
+              !isRendered && "opacity-0 scale-105",
+            ],
+            className,
+          )}
+        >
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
