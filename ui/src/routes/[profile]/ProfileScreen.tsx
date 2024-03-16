6636c4e262c2f945bfee1dea8908e6744910fae4
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-03-17 07:52:36 +0800

    Conditionally render profile posts section

--- ui/src/routes/[profile]/ProfileScreen.tsx
+++ ui/src/routes/[profile]/ProfileScreen.tsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
 import { useProfileLoader } from "../[profile]";
 import { Screen } from "../_app/Screen";
+import { useUserContext } from "../_app/UserContext";
 import { NavBar } from "./NavBar";
 import { ProfileCard } from "./ProfileCard";
 import { PostsProvider } from "./posts/PostsContext";
 import { PostsList } from "./posts/PostsList";
 
 function PostsSection() {
+  const { user } = useUserContext();
+  const { profile, followers } = useProfileLoader();
+
+  const isProfilePublic = !profile.isPrivate;
+  const isFollowing =
+    followers?.some(({ follower }) => follower.id === user?.id) ?? false;
+  const canShowPosts = isProfilePublic || isFollowing;
+
+  if (!canShowPosts) {
+    return null; // TODO Render informational card
+  }
+
   return (
     <section>
       <h2 className="sr-only">Spills 🍵</h2>
