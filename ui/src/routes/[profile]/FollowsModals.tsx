9f18e3464b354d4ad1afb25bfdebad52fca6abcc
Mark Poso <63066834+mark-P0@users.noreply.github.com>
2024-03-20 10:28:04 +0800

    Initialize requesting user card controls

--- ui/src/routes/[profile]/FollowsModals.tsx
+++ ui/src/routes/[profile]/FollowsModals.tsx
@@ -1,7 +1,7 @@
 import { UserPublic } from "@spill-it/db/schema/drizzle";
 import clsx from "clsx";
 import { useEffect } from "react";
-import { BsPersonPlusFill, BsXLg } from "react-icons/bs";
+import { BsCheck, BsPersonPlusFill, BsX, BsXLg } from "react-icons/bs";
 import { Link, useNavigate } from "react-router-dom";
 import { endpointWithParam } from "../../utils/endpoints";
 import { useProfileLoader } from "../[profile]";
@@ -36,6 +36,42 @@ function UserCard(props: { user: UserPublic }) {
   );
 }
 
+// TODO Reuse existing user card?
+function RequestingUserCard(props: { user: UserPublic }) {
+  const { user } = props;
+
+  const { handleName, username, portraitUrl } = user;
+  return (
+    <article className="flex items-center gap-3 rounded p-3 bg-white/10">
+      <img
+        src={portraitUrl}
+        alt={`Portrait of "${handleName}"`}
+        className="w-9 aspect-square rounded-full"
+      />
+      <header>
+        <h3>
+          <Link
+            to={endpointWithParam("/:username", { username })}
+            className={clsx("font-bold", clsLink)}
+          >
+            {handleName}
+          </Link>
+        </h3>
+        <p className="text-white/50 text-sm">{username}</p>
+      </header>
+
+      <div className="ml-auto flex flex-row-reverse items-center">
+        <button className={clsx(clsBtnIcon, "enabled:hover:!bg-red-700")}>
+          <BsX className="w-full h-full" />
+        </button>
+        <button className={clsx(clsBtnIcon, "enabled:hover:!bg-fuchsia-600")}>
+          <BsCheck className="w-full h-full" />
+        </button>
+      </div>
+    </article>
+  );
+}
+
 function FollowerRequestsModalContent() {
   const { followerRequests } = useProfileLoader();
   const { closeModal } = useModalContext();
@@ -56,8 +92,7 @@ function FollowerRequestsModalContent() {
 
       <ol className="mt-3 grid gap-1">
         {followerRequests?.map(({ follower }) => (
-          // TODO Add controls for accepting/rejecting requests
-          <UserCard key={follower.id} user={follower} />
+          <RequestingUserCard key={follower.id} user={follower} />
         ))}
       </ol>
     </ModalContent>
